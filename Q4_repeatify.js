75291f520d4f72c66bb3af4eeae19c13bdfd1423
Bin Liu <bin.w.liu@gmail.com>
2016-09-06 20:29:40 -0400

    Add Q4

---
+++ Q4_repeatify.js
@@ -0,0 +1,28 @@
+/*
+Define a repeatify function on the String object. The function accepts an integer that specifies how many times the string has to be repeated. The function returns the string repeated the number of times specified. For example:
+
+console.log('hello'.repeatify(3));
+
+Should print hellohellohello.*/
+
+String.Prototype.repeatify = function(n){
+	var concatedstr='';
+	if (this != null){
+		for (int i = 0; i<n; i++){
+			concatedstr=concatedstr.concat(this);
+		}
+		return concatedstr;
+	}
+	return null;
+}
+
+
+String.prototype.repeatify = String.prototype.repeatify || function(times) {
+   var str = '';
+
+   for (var i = 0; i < times; i++) {
+      str += this;
+   }
+
+   return str;
+};
